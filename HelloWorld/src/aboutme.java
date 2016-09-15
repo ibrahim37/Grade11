4859db1153d7436b59b83f6e63f8b038d990849b
324067404 <324067404@C-9R62YR1.tdsb.on.ca>
2016-09-15 13:11:04 -0400

    This my about me

---
+++ HelloWorld/src/aboutme.java
@@ -0,0 +1,25 @@
+/*aboutme.java
+ * printing about my schedule 
+ * 14/9/16
+ * Ibrahim
+ * 
+ */
+public class aboutme {
+	/**
+	 * @param args
+	 */
+	public static void main(String[]args){
+		System.out.format("%-10s %8s %8s","Period","day1","day2\n");
+		System.out.format("%-10s %8s %8s","1","BAF3M1-01","BAF3M1-01\n");
+	    System.out.format("%-10s %8s %8s","2","CGG301-01","CGG301-01\n");
+		System.out.format("%-10s %8s %8s","3","ICS3U1-01","ICS3U1-01\n");
+		System.out.format("%-10s %8s %8s","4","PPL3OM-01","PPL3OM-01\n");
+		
+
+	}   
+
+
+	
+	
+
+}
