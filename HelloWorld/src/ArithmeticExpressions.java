826ba48dcaecc3dabe95003692f49366981ce8ce
324067404 <324067404@C-9R62YR1.tdsb.on.ca>
2016-09-28 13:22:36 -0400

    My Arithmetic

---
+++ HelloWorld/src/ArithmeticExpressions.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author 324067404
+ *
+ */
+public class ArithmeticExpressions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	 Scanner scan = new Scanner(System.in);
+	 int num1, num2, num3, num4, num5;
+	 
+	 System.out.println("Enter 5 number");
+	 
+	 num1= scan.nextInt();
+	 num2= scan.nextInt();
+	 num3= scan.nextInt();
+	 num4= scan.nextInt();
+	 num5= scan.nextInt();
+	 
+	 System.out.println("Your amount is:  " +(num1 + num2 + num3 + num4 + num5));
+	 System.out.println("Second number - Third number is: "+(num2 - num3));
+	 System.out.println("The product of the first and fifth numbers is "+(num1 * num5));
+	 System.out.println("The quotient of the fourth number divided by the second " +(num4/num2));
+	 System.out.println("The remainder when dividing the fourth number by the second " +(num4%num2));
+	 
+	 
+
+	}
+	}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+				 
+			 
+	 		
+
+	
+
+
