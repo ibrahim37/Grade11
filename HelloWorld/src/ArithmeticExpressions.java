c51b9c04c27318e1cad2383e79fcbb13ebba48b3
324067404 <324067404@C-9R62YR1.tdsb.on.ca>
2016-09-30 12:46:47 -0400

    My arithmetic expression for 6 and 7

--- HelloWorld/src/ArithmeticExpressions.java
+++ HelloWorld/src/ArithmeticExpressions.java
@@ -30,8 +30,17 @@ public class ArithmeticExpressions {
 	 System.out.println("The product of the first and fifth numbers is "+(num1 * num5));
 	 System.out.println("The quotient of the fourth number divided by the second " +(num4/num2));
 	 System.out.println("The remainder when dividing the fourth number by the second " +(num4%num2));
+	 System.out.println("The first number raised to the power of the third number " +(Math.pow(num1,num3)));
+	 System.out.println("The square root of the fifth number " +(Math.sqrt(num5)));
 	 
 	 
+			 
+
+	 
+	 
+	 
+	 
+
 
 	}
 	}
